1a5b36850bf0c06328b5cb147aaea9da7646e5e8
Michael Levi <michaellevi151@gmail.com>
2021-03-05 02:39:59 -0500

    Refactor to take an object at the top of the file for every command format that is to be added

--- Plugin/src/main/java/com/sonelli/juicessh/performancemonitor/controllers/TemperatureController.java
+++ Plugin/src/main/java/com/sonelli/juicessh/performancemonitor/controllers/TemperatureController.java
@@ -11,6 +11,7 @@ import com.sonelli.juicessh.pluginlibrary.exceptions.ServiceNotConnectedExceptio
 import com.sonelli.juicessh.pluginlibrary.listeners.OnSessionExecuteListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +21,29 @@ import java.util.regex.Pattern;
 public class TemperatureController extends BaseController {
 
     public static final String TAG = "TemperatureController";
+
+    final List<TemperatureCommandModel> temperatureCommandModels = Arrays.asList(
+            new TemperatureCommandModel("/opt/vc/bin/vcgencmd measure_temp",
+                    Pattern.compile("temp=(\\S+)"),
+                    new TextFormatter() {
+                        @Override
+                        public String format(Pattern p, String line) {
+                            return p.matcher(line).group(1);
+                        }
+                    }),
+            new TemperatureCommandModel("cat /sys/class/thermal/thermal_zone0/temp",
+                    Pattern.compile("(\\d{5})"),
+                    new TextFormatter() {
+                        @Override
+                        public String format(Pattern p, String line) {
+                            int intLine = Integer.parseInt(line);
+                            double doubleTemp = intLine / 1000;
+                            return doubleTemp + "°C";
+                        }
+                    })
+    );
+
+    List<TemperatureCommandModel> validTemperatureCommandModels = new ArrayList<>();
 
     public TemperatureController(Context context) {
         super(context);
@@ -31,85 +55,44 @@ public class TemperatureController extends BaseController {
 
         // Work out the temperature of the device
 
-        final List<TemperatureCommandModel> temperatureCommandModels = new ArrayList<>();
-
         // First, let do some service discovery
+        // Check which of the commands in temperatureCommandModels is valid
 
         new Handler().post(new Runnable() {
             @Override
             public void run() {
 
-                try {
-                    // Check first command
-                    final Pattern vcgPattern = Pattern.compile("temp=(\\S+)");
-                    final String vcgCommand = "/opt/vc/bin/vcgencmd measure_temp";
-
-                    getPluginClient().executeCommandOnSession(getSessionId(), getSessionKey(), vcgCommand, new OnSessionExecuteListener() {
-                        @Override
-                        public void onCompleted(int exitCode) {
-                            switch(exitCode){
-                                case 127:
-                                    Log.d(TAG, vcgCommand + " was not found on the server");
+                for (final TemperatureCommandModel t : temperatureCommandModels) {
+                    try {
+                        getPluginClient().executeCommandOnSession(getSessionId(), getSessionKey(), t.getCommand(), new OnSessionExecuteListener() {
+                            @Override
+                            public void onCompleted(int exitCode) {
+                                switch(exitCode){
+                                    case 127:
+                                        Log.d(TAG, t.getCommand() + " was not found on the server");
+                                }
                             }
-                        }
-                        @Override
-                        public void onOutputLine(String line) {
-                            Matcher vcgMatcher = vcgPattern.matcher(line);
-                            if(vcgMatcher.find()){
-                                temperatureCommandModels.add(new TemperatureCommandModel(vcgCommand, vcgPattern, new TextFormatter() {
-                                    @Override
-                                    public String format(Pattern pattern, String line) {
-                                        return pattern.matcher(line).group(1);
-                                    }
-                                }));
+
+                            @Override
+                            public void onOutputLine(String s) {
+                                Matcher isValidChecker = t.getPattern().matcher(s);
+                                if (isValidChecker.find()) {
+                                    // This is a valid command - add to our list of valid commands
+                                    validTemperatureCommandModels.add(t);
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onError(int error, String reason) {
-                            toast(reason);
-                        }
-                    });
-
-
-                    // Check second command
-                    final Pattern sysPattern = Pattern.compile("(\\d{5})");
-                    final String sysCommand = "cat /sys/class/thermal/thermal_zone0/temp";
-
-                    getPluginClient().executeCommandOnSession(getSessionId(), getSessionKey(), sysCommand, new OnSessionExecuteListener() {
-                        @Override
-                        public void onCompleted(int exitCode) {
-                            switch(exitCode){
-                                case 127:
-                                    Log.d(TAG, "Tried to run a command but the command was not found on the server");
+                            @Override
+                            public void onError(int i, String reason) {
+                                toast(reason);
                             }
-                        }
-                        @Override
-                        public void onOutputLine(String line) {
-                            Matcher sysMatcher = sysPattern.matcher(line);
-                            if (sysMatcher.find()) {
-                                temperatureCommandModels.add(new TemperatureCommandModel(sysCommand, sysPattern, new TextFormatter() {
-                                    @Override
-                                    public String format(Pattern pattern, String line) {
-                                        int intLine = Integer.parseInt(line);
-                                        double doubleTemp = intLine / 1000;
-                                        return doubleTemp + "°C";
-                                    }
-                                }));
-                            }
-                        }
-
-                        @Override
-                        public void onError(int error, String reason) {
-                            toast(reason);
-                        }
-                    });
-
-                } catch (ServiceNotConnectedException e) {
-                    Log.d(TAG, "Tried to execute a command but could not connect to JuiceSSH plugin service");
+                        });
+                    } catch (ServiceNotConnectedException e) {
+                        Log.e(TAG, "Tried to execute a command but could not connect to JuiceSSH plugin service");
+                    }
                 }
-
             }
+
         });
 
 
@@ -120,13 +103,13 @@ public class TemperatureController extends BaseController {
             @Override
             public void run() {
 
-                if (isRunning() && temperatureCommandModels.isEmpty()){
+                if (isRunning() && validTemperatureCommandModels.isEmpty()){
                     handler.postDelayed(this, INTERVAL_SECONDS * 1000L);
                     return;
                 }
 
                 try {
-                    final TemperatureCommandModel temperatureCommandModel = temperatureCommandModels.get(0);
+                    final TemperatureCommandModel temperatureCommandModel = validTemperatureCommandModels.get(0);
                     getPluginClient().executeCommandOnSession(getSessionId(), getSessionKey(), temperatureCommandModel.getCommand(), new OnSessionExecuteListener() {
                         @Override
                         public void onCompleted(int exitCode) {
